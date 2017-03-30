ebe3644f558396cb5b473304940c1434bcef3aaf
cjredmond <connor.redmond@gmail.com>
2017-03-30 16:11:38 -0400

    initial

---
+++ james_app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from user_auth.views import UserCreateView
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
+
+]
