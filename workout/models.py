c4e7cf90ede7971f1fd872aaf56eff6673b45a2a
cjredmond <connor.redmond@gmail.com>
2017-04-24 14:12:56 -0400

    add archive field to workout model

--- workout/models.py
+++ workout/models.py
@@ -6,14 +6,4 @@ class Workout(models.Model):
     name = models.CharField(max_length=30)
     info = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
-
-# MUSCLE_GROUPS = [('q', 'quads'), ('h', 'hamstrings'), ('c', 'calves'), ('a', 'abs'),
-#                  ('p', 'pecs'), ('s', 'shoulders'), ('l', 'lats'), ('b', 'biceps'),
-#                  ('t', 'triceps'), ('m', 'mobility')]
-# class Exercise(models.Model):
-#     workout = models.ForeignKey(Workout)
-#     name = models.CharField(max_length=40)
-#     muscle_group = models.CharField(max_length=1,choices=MUSCLE_GROUPS)
-#     sets = models.CharField(max_length=10)
-#     reps = models.CharField(max_length=10)
-#     notes = models.CharField(max_length=100,null=True,blank=True)
+    archive = models.BooleanField(default=False)
